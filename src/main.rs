4df189825f308871dff0dd875e9ed00f91cc566e
wyatthlang <wyatthlang@gmail.com>
2025-10-25 22:25:27 -0400

    fixed integer overflow issue

--- src/main.rs
+++ src/main.rs
@@ -62,11 +62,11 @@ impl App {
     }
 
     fn increment_counter(&mut self) {
-        self.counter += 1;
+        self.counter = self.counter.saturating_add(1);
     }
 
     fn decrement_counter(&mut self) {
-        self.counter -= 1;
+        self.counter = self.counter.saturating_sub(1);
     }
 
 }
